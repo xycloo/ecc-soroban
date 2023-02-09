7932e375a15687c5cc51fc00204336925abe55d9
Graydon Hoare <graydon@pobox.com>
2023-02-08 21:09:06 -0800

    Split prove from verify using serialization, add test

--- groth-16-verifier/src/test.rs
+++ groth-16-verifier/src/test.rs
@@ -1,1 +1,10 @@
+use soroban_sdk::Env;
+use crate::*;
 
+#[test]
+fn verify_test()
+{
+    let env = Env::default();
+    let proof_bytes = TestVerifier::prove(env.clone());
+    TestVerifier::verify(env, proof_bytes);  
+}
