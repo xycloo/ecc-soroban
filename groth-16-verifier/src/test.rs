925648be454bded7aaf6fe283dbdc8eba86994a9
heytdep <tommasodeponti@zohomail.eu>
2023-02-09 11:43:15 +0100

    split verifiyng key build through serialization

--- groth-16-verifier/src/test.rs
+++ groth-16-verifier/src/test.rs
@@ -1,10 +1,17 @@
-use soroban_sdk::Env;
 use crate::*;
+use soroban_sdk::Env;
+extern crate std;
 
 #[test]
-fn verify_test()
-{
+fn verify_test() {
     let env = Env::default();
     let proof_bytes = TestVerifier::prove(env.clone());
-    TestVerifier::verify(env, proof_bytes);  
+    let key_bytes = TestVerifier::get_key(env.clone());
+
+    std::println!("{:?} \n\n", proof_bytes);
+    std::println!("{:?}", key_bytes);
+
+    let res = TestVerifier::verify(env, key_bytes, proof_bytes);
+
+    assert!(res);
 }
